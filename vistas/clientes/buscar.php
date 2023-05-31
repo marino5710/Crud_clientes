5e1ba44c801d6059aa82802cba126a0d0519f711
marino5710 <brianmarin4@gmail.com>
2023-05-31 09:59:55 -0600

    se creo el archivo buscar

---
+++ vistas/clientes/buscar.php
@@ -0,0 +1,8 @@
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Buscar clientes</h1>
+        <div class="row justify-content-center">
+            <form action="/practica_8/controladores/clientes/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
