5203279159eec87c5d71c2784c25a5ed93fb150f
marino5710 <brianmarin4@gmail.com>
2023-05-31 09:45:02 -0600

    se creo el html

--- controladores/clientes/modificar.php
+++ controladores/clientes/modificar.php
@@ -27,3 +27,37 @@ if($_POST['cliente_nombre'] != '' && $_POST['cliente_nit']  != '' && $_POST['cli
 // }
 
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Resultados</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if($resultado): ?>
+                    <div class="alert alert-success" role="alert">
+                        Modificado exitosamente!
+                    </div>
+                <?php else :?>
+                    <div class="alert alert-danger" role="alert">
+                        Ocurrió un error: <?= $error ?>
+                    </div>
+                <?php endif ?>
+              
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="/practica_8/controladores/clientes/buscar.php?cliente_nombre=<?= $_POST['cliente_nombre'] ?>" class="btn btn-info">Volver al formulario</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
