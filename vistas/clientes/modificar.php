d3ee1cf6fc2d6ff0d30442157810420cbd0e2e55
marino5710 <brianmarin4@gmail.com>
2023-05-31 10:02:15 -0600

    se creoi el archivo modificar

---
+++ vistas/clientes/modificar.php
@@ -0,0 +1,17 @@
+<?php
+require '../../modelos/Cliente.php';
+    try {
+        $cliente = new Cliente($_GET);
+
+        $clientes = $cliente->buscar();
+        // echo "<pre>";
+        // var_dump($clientes[0]['CLIENTE_ID']);
+        // echo "</pre>";
+        // exit;
+        // $error = "NO se guardó correctamente";
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    } catch (Exception $e2){
+        $error = $e2->getMessage();
+    }
+?>
