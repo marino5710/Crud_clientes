f8c3886c6698b64bf6cc6e61b580d5dbdb684aed
marino5710 <brianmarin4@gmail.com>
2023-05-31 10:08:47 -0600

    se creo la tabla clientes

---
+++ bd.sql
@@ -0,0 +1,6 @@
+CREATE TABLE clientes (
+    cliente_id serial primary key not null,
+    cliente_nombre varchar(60) not null,
+    cliente_nit varchar(10) not null,
+    cliente_situacion smallint not null default 1
+);
